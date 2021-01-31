491096ce2859b7f35f554fe9efa3150229b514fa
willian-lemann <willianleman@hotmail.com>
2021-01-31 14:06:39 -0300

    feat: add new components

--- src/pages/abner/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const AbnerFonseca: React.FC = () => {
-  return <h1>Abner Fonseca</h1>;
-};
-
-export default AbnerFonseca;
